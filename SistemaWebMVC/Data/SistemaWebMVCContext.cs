22f08b009afa5b73f645f1586e39c70a340217a6
douglashassislima <douglashenriquedeassis@gmail.com>
2021-09-11 16:12:09 -0300

    Solving errors and CRUD Scaffolding

---
+++ SistemaWebMVC/Data/SistemaWebMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SistemaWebMVC.Models;
+
+namespace SistemaWebMVC.Data
+{
+    public class SistemaWebMVCContext : DbContext
+    {
+        public SistemaWebMVCContext (DbContextOptions<SistemaWebMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SistemaWebMVC.Models.Department> Department { get; set; }
+    }
+}
--- SistemaWebMVC/Startup.cs
+++ SistemaWebMVC/Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SistemaWebMVC.Data;
 
 namespace SistemaWebMVC
 {
@@ -33,6 +35,9 @@ namespace SistemaWebMVC
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<SistemaWebMVCContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SistemaWebMVCContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
